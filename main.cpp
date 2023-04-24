fa5218e8e2a1450495fb4770b00ca65a52184c1e
Blindello <129384662+Blindello@users.noreply.github.com>
2023-04-24 15:51:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
-   return 0;
+int n, K;
+int a=0;
+int b=0;
+int i=0;
+    cin>>n;
+    while (n<=2) {
+        cout<<"errore"<<endl;
+        cin>>n;
+
+    }
+    K=n;
+    for(i=0;i<K;i++){
+    b=n+a;
+    n=a;
+    a=b;
+    cout<<b<<endl;
+    }
+    return 0;
 }
