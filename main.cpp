7c495ee55d7a3da9dc47659eaa27423ca83f7e22
Blindello <129384662+Blindello@users.noreply.github.com>
2023-04-27 20:28:47 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,19 +4,20 @@ using namespace std;
 int main() {
 int n, K;
 int a=0;
-int b=0;
+int b=1;
 int i=0;
     cin>>n;
     if (n>=2) {
     K=n;
     while (i<K) {
     i++;
-    b=n+a;
     n=a;
     a=b;
     cout<<b<<endl;
+    b=n+a;
     }}
     else {
         cout<<"errore"<<endl;
+        }
     return 0;
 }
